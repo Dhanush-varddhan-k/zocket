b1cad738ba643a732026330bc910e8173b7da6c9
Dhanush varddhan K <114069009+Dhanush-varddhan-k@users.noreply.github.com>
2024-11-29 13:25:14 +0530

    Update products.go

--- models/products.go
+++ models/products.go
@@ -1,43 +1,4 @@
-// package models
 
-// import "gorm.io/gorm"
-
-// type Product struct {
-// 	ID                      uint     `gorm:"primaryKey;autoIncrement" json:"id"`
-// 	UserID                  uint     `json:"user_id"`
-// 	ProductName             string   `json:"product_name"`
-// 	ProductDescription      string   `json:"product_description"`
-// 	ProductImages           []string `gorm:"type:text[]" json:"product_images"`
-// 	ProductPrice            float64  `json:"product_price"`
-// 	CompressedProductImages []string `gorm:"type:text[]" json:"compressed_product_images"`
-// }
-
-// func Migrate(db *gorm.DB) error {
-// 	err := db.AutoMigrate(&Product{})
-// 	return err
-// }
-
-// package models
-
-// import "gorm.io/gorm"
-
-// type Product struct {
-// 	ID                      uint     `gorm:"primaryKey;autoIncrement" json:"id"`
-// 	UserID                  uint     `json:"user_id"`
-// 	ProductName             string   `json:"product_name"`
-// 	ProductDescription      string   `json:"product_description"`
-// 	ProductImages           []string `gorm:"-" json:"product_images"` // Use slice for JSON unmarshalling
-// 	ProductPrice            float64  `json:"product_price"`
-// 	CompressedProductImages []string `gorm:"-" json:"compressed_product_images"`
-// 	ProductImagesJSON       string   `gorm:"type:json" json:"-"` // Store as JSON in DB
-// 	CompressedImagesJSON    string   `gorm:"type:json" json:"-"` // Store as JSON in DB
-// }
-
-//	func Migrate(db *gorm.DB) error {
-//		db.Migrator().DropTable(&Product{})
-//		err := db.AutoMigrate(&Product{})
-//		return err
-//	}
 package models
 
 import "gorm.io/gorm"
